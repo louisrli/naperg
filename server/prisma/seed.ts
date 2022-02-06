d09740573224dd851e0ea2b5692a22750c3975b5
Sergei Mikharevich <mikharevichsn@mail.ru>
2022-02-06 13:03:59 +0300

    seeder1

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,27 +1,39 @@
-// import { PrismaClient } from '@prisma/client'
-// import * as bcrypt from 'bcryptjs'
+import { PrismaClient } from '@prisma/client'
+import * as bcrypt from 'bcryptjs'
+import { faker } from '@faker-js/faker'
 
-// const prisma = new PrismaClient()
-// async function main() {
-//   const user = await prisma.user.create({
-//     data: {
-//       email: `admin@naperg.com`,
-//       name: 'Admin Naperg',
-//       password: await bcrypt.hash('admin', 10),
-//       role: 'ADMIN',
-//       resetPasswordToken: '123',
-//       validateEmailToken: '',
-//       isEmailValidated: true,
-//     },
-//   })
+const prisma = new PrismaClient()
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      id: 5,
+      firstName: 'Ivan',
+      lastName: 'Ivanov',
+      // fullName: 'Ivan Ivanov',
+      email: faker.internet.email(),
+    },
+  })
+  console.log({ user })
 
-//   console.log({ user })
-// }
-// main()
-//   .catch((e) => {
-//     console.error(e)
-//     process.exit(1)
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect()
-//   })
+  for (let i = 1; i < 5; i += 1) {
+    const feed = await prisma.feed.create({
+      data: {
+        id: i,
+        userId: 1,
+        isFavorite: faker.datatype.boolean(),
+      },
+    })
+  }
+
+  // for (let i = 0; i < 20; i += 1) {
+  //   const feed =
+  // }
+}
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
