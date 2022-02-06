b46661e07a9166d7ab945efd4b5f3700aab2fd97
Sergei Mikharevich <mikharevichsn@mail.ru>
2022-02-06 14:17:47 +0300

    seeder3

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -4,30 +4,85 @@ import { faker } from '@faker-js/faker'
 
 const prisma = new PrismaClient()
 async function main() {
+  await prisma.post.deleteMany()
+  await prisma.sourceFeedRelations.deleteMany()
+  await prisma.source.deleteMany()
+  await prisma.feed.deleteMany()
+  await prisma.user.deleteMany()
+
   const user = await prisma.user.create({
     data: {
       id: 5,
-      firstName: 'Ivan',
-      lastName: 'Ivanov',
-      // fullName: 'Ivan Ivanov',
+      fullName: 'Ivan Ivanov',
       email: faker.internet.email(),
     },
   })
   console.log({ user })
 
+  // создаю фиды
   for (let i = 1; i < 5; i += 1) {
     const feed = await prisma.feed.create({
       data: {
         id: i,
-        userId: 1,
+        userId: 5,
         isFavorite: faker.datatype.boolean(),
       },
     })
   }
 
-  // for (let i = 0; i < 20; i += 1) {
-  //   const feed =
-  // }
+  // создаю сорцы
+  for (let i = 1; i < 4; i += 1) {
+    const domainName = faker.internet.domainName()
+    const source = await prisma.source.create({
+      data: {
+        id: i,
+        title: domainName,
+        url: `http://${domainName}`,
+      },
+    })
+  }
+
+  // sourceFeedRelations
+
+  // у первого фида все 3 сорца
+  for (let i = 1; i < 4; i += 1) {
+    await prisma.sourceFeedRelations.create({
+      data: {
+        sourceId: i,
+        feedId: 1,
+      },
+    })
+  }
+
+  // у второго фида только второй сорц (1 шт)
+  await prisma.sourceFeedRelations.create({
+    data: {
+      sourceId: 2,
+      feedId: 2,
+    },
+  })
+
+  // у третьего фида НЕТУ сорцов
+
+  //   /sourceFeedRelations
+
+  // posts
+
+  for (let i = 1; i < 4; i += 1) {
+    for (let x = 1; x < 11; x += 1) {
+      await prisma.post.create({
+        data: {
+          title: faker.lorem.sentence(),
+          content: faker.lorem.paragraphs(4),
+          imgUrl: faker.image.imageUrl(),
+          url: faker.internet.url(),
+          sourceId: i,
+        },
+      })
+    }
+  }
+
+  // /posts
 }
 main()
   .catch((e) => {
