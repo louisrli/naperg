d6051ef31f4ce64647ac62189a274a03cace3b4a
Daria Kliukhina <dariasergeevna90@gmail.com>
2022-02-13 15:29:28 +0300

    add sources endpoint + rename endpoints

--- server/src/typeDefs/query.ts
+++ server/src/typeDefs/query.ts
@@ -2,9 +2,10 @@ import { gql } from 'apollo-server';
 
 export const query = gql`
     type Query {
-        user: User!
-        getUser(userId: Int): User!
-        getUserSettings(userId: Int): UserSetting!
+        # user: User!
+        user(userId: Int): User!
+        userSettings(userId: Int): UserSetting!
+        sources: [Source]
 
         #    usersPagination(page: Float!, where: UserWhereInput): UsersPagination!
         #    user(userId: String!): User!
