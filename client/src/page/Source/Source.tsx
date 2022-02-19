3a5e5d15ff074fe442f6074d55b10d6f180cd96c
Sergei Mikharevich <mikharevichsn@mail.ru>
2022-02-19 15:18:14 +0300

    apollo client

--- client/src/page/Source/Source.tsx
+++ client/src/page/Source/Source.tsx
@@ -1,53 +1,45 @@
-import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
-import { gql, request } from 'graphql-request';
-import { Urls } from '../../lib/urls';
+import { useQuery, gql } from '@apollo/client';
 
 export function Source() {
-  const [posts, setPosts] = useState([]);
   const { id } = useParams();
   const navigate = useNavigate();
 
+  const query = gql`
+    query SourcePostsPaginated($sourceId: Int, $total: Int, $page: Int) {
+      sourcePostsPaginated(sourceId: $sourceId, total: $total, page: $page) {
+        id
+        title
+      }
+    }
+  `;
 
-  useEffect(() => {
-    (async () => {
-      const query = gql`
-          query SourcePostsPaginated($sourceId: Int, $total: Int, $page: Int) {
-              sourcePostsPaginated(sourceId: $sourceId, total: $total, page: $page) {
-                  id
-                  title
-              }
-          }
-      `;
+  const variables = {
+    sourceId: parseInt(id, 10),
+    total: 10,
+    page: 1,
+  };
 
-      // TODO `avoid magic number and implement select total and page`
-      const variables = {
-        sourceId: parseInt(id, 10),
-        total: 10,
-        page: 1,
-      };
+  const { loading, error, data } = useQuery(query, { variables });
 
-      const response = await request(Urls.graphql, query, variables);
-      setPosts(response?.sourcePostsPaginated || []);
-    })();
-  }, []);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
   return (
     <div>
       <ul>
-        {
-          posts.map(post =>
-            <li key={post.id}>
-              <h4>
-                {post.title}
-              </h4>
-              <button onClick={() => {
+        {data?.sourcePostsPaginated.map((post) => (
+          <li key={post.id}>
+            <h4>{post.title}</h4>
+            <button
+              onClick={() => {
                 navigate(`/posts/${post.id}`);
-              }}>
-                read more
-              </button>
-            </li>,
-          )}
+              }}
+            >
+              read more
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
