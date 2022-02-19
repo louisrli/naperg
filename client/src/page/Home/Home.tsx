bbaf5ed5e9b9e037e4728cf4e0f244f8e31ff154
Daria Kliukhina <dariasergeevna90@gmail.com>
2022-02-19 17:33:53 +0300

    add headlines

--- client/src/page/Home/Home.tsx
+++ client/src/page/Home/Home.tsx
@@ -1,8 +1,35 @@
+import { useQuery, gql } from '@apollo/client';
+import {PostPreview} from '../../components/PostPreview/PostPreview'
+
 export function Home() {
+  
+  const query = gql`
+  query Query {
+    headlines {
+      title
+      id
+    }
+  }
+  `;
+  
+  const { loading, error, data } = useQuery(query);
+  
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
     <div>
       <div>
         <p>Hello, that's super application here you read awesome rss feeds</p>
+        <h2>Headlines</h2>
+        <ul>
+        {data?.headlines.map((post) => (
+          <li key={post.id}>
+            {/* @ts-ignore*/}
+            <PostPreview title={post.title} id={post.id}/>
+          </li>
+        ))}
+      </ul>
       </div>
     </div>
   );
